67eb27c54bfc943159a34866a338334f4e213a31
Ahmed Karam <ahmedkarm749@gmail.com>
2023-04-17 23:50:43 +0200

    initail commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-sky-500">
+      <h1 className="text-4xl text-center p-2">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;

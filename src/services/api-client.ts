e229b9bccb57bfb86200c2df9638a559e3b52669
Ahmed Karam <ahmedkarm749@gmail.com>
2023-07-22 00:31:56 +0300

    edit api end point

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://api.rawg.io/api",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "aaa61a5dc95148adaeabc0ce39018296",
   },
